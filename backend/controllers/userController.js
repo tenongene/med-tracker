a9166591f5008093c81c489e266b7d62cb8dd7b7
Tenongene <tenongene00@gmail.com>
2023-11-24 04:00:05 -0600

    Querycommand and PutItemcommand complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const _ = require('lodash');
 const { randomInt } = require('node:crypto');
+const bcrypt = require('bcrypt');
+
+//bccrypt hashing
+const hashPass = async (password) => {
+	const salt = await bcrypt.genSalt();
+	const hash = await bcrypt.hash(password, salt);
+	return hash;
+};
+
+//database client
 const {
 	DynamoDBClient,
 	PutItemCommand,
 	ScanCommand,
 	DeleteItemCommand,
 	UpdateItemCommand,
-	GetItemCommand,
+	QueryCommand,
 } = require('@aws-sdk/client-dynamodb');
 
 const id = () => {
@@ -18,7 +28,7 @@ const id = () => {
 const ACCESS_KEY = process.env.ACCESS_KEY;
 const SECRET = process.env.SECRET;
 const REGION = process.env.REGION;
-const TABLE_NAME2 = process.env.TABLE_NAME;
+const TABLE_NAME = process.env.TABLE_NAME2;
 const client = new DynamoDBClient({
 	region: REGION,
 	accessKeyId: ACCESS_KEY,
@@ -27,53 +37,66 @@ const client = new DynamoDBClient({
 
 // login user
 const loginUser = (req, res) => {
-	res.send({ msg: 'User logged in!' });
-	// const user = new GetItemCommand({
-	// 	TableName: TABLE_NAME2,
-	// 	Key: { email: { S: req.params.email } },
-	// });
+	const { email } = req.body;
+	console.log(email);
+	const user = new QueryCommand({
+		TableName: TABLE_NAME,
+		KeyConditionExpression: 'email = :emailval',
+		Select: 'ALL_ATTRIBUTES',
+		ExpressionAttributeValues: {
+			':emailval': {
+				S: email,
+			},
+		},
+	});
 
-	// client
-	// 	.send(user)
-	// 	.then((response) => {
-	// 		res.status(200).json(response.Item);
-	// 		console.log(response.Item);
-	// 	})
-	// 	.catch((err) => {
-	// 		res.status(404).json({ error: err.message });
-	// 		console.log(err.message);
-	// });
+	client
+		.send(user)
+		.then((response) => {
+			res.status(200).json(response.Items);
+			console.log(response.Items);
+		})
+		.catch((err) => {
+			res.status(404).json({ error: err.message });
+			console.log(err.message);
+		});
 };
 
 // signup user
 const signupUser = (req, res) => {
-	res.send({ msg: 'User signed up!' });
-	// const { userName, email, password, firstName } = req.body;
-	// const userModel = {
-	// 	TableName: TABLE_NAME2,
-	// 	Item: {
-	// 		userName: {
-	// 			S: `${userName}`,
-	// 		},
-	// 		password: `${password}`,
-	// 		email: `${email}`,
-	// 		firstName: `${firstName}`,
-	// 		id: {
-	// 			N: `${id()}`,
-	// 		},
-	// 	},
-	// };
-	// const newUser = new PutItemCommand(userModel);
-	// client
-	// 	.send(newUser)
-	// 	.then((response) => {
-	// 		res.status(200).json(response);
-	// 		console.log(response);
-	// 	})
-	// 	.catch((err) => {
-	// 		res.status(500).json({ error: err.message });
-	// 		console.log(err.message);
-	// 	});
+	console.log(req.body);
+	const { userName, email, password, firstName } = req.body;
+	const userModel = {
+		TableName: TABLE_NAME,
+		Item: {
+			userName: {
+				S: `${userName}`,
+			},
+			password: {
+				S: `${password}`,
+			},
+			email: {
+				S: `${email}`,
+			},
+			firstName: {
+				S: `${firstName}`,
+			},
+			id: {
+				N: `${id()}`,
+			},
+		},
+	};
+	const newUser = new PutItemCommand(userModel);
+	client
+		.send(newUser)
+		.then((response) => {
+			res.status(200).json(response);
+			console.log(response);
+		})
+		.catch((err) => {
+			res.status(500).json({ error: err.message });
+			console.log(err);
+		});
 };
 
 module.exports = { signupUser, loginUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,8 +5,10 @@ const { signupUser, loginUser } = require('../controllers/userController');
 const router = express.Router();
 
 //login route
-router.post('/login', loginUser),
-	//signup route
-	router.post('/signup', signupUser),
-	///
-	(module.exports = router);
+router.post('/login', loginUser);
+
+//signup route
+router.post('/signup', signupUser);
+
+///
+module.exports = router;
