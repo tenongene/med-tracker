11c67f3fbd7ce15313744d43331e20f058c52aca
Tenongene <tenongene00@gmail.com>
2023-11-25 14:40:35 -0600

    Bs login/signup forms complete

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -13,13 +13,23 @@ const Login = () => {
 		<div className="container ">
 			<div className="row justify-content-center ">
 				<div className="col-sm-12 col-md-8 col-lg-10">
-					<div className="card">
-						<div>
-							<img src="mt3.jpg" className="entryLogo" alt="logo" />
-							<h2>MedTracker</h2>
+					<div className="card border border-info-subtle rounded-4 shadow-lg">
+						<div className="container">
+							<div className="row">
+								<img src="mt3.jpg" className="entryLogo" alt="logo" />
+
+								<div className="col  align-self-center">
+									<h1 className="fw-bold d-inline-flex m-5" style={{ textDecoration: 'none', color: 'darkcyan' }}>
+										<em>MedTracker</em>
+									</h1>
+									<h5 className="pt-2">
+										<em>Manage and track all your medications seamlessly!</em>
+									</h5>
+								</div>
+							</div>
 						</div>
 						<div className="card-body">
-							<form>
+							<form className="p-3">
 								<div className="mb-3">
 									<label htmlFor="inputEmail" className="form-label">
 										Email address
@@ -45,7 +55,7 @@ const Login = () => {
 										value={password}
 									/>
 								</div>
-								<button type="submit" className="btn btn-success" onSubmit={handleSubmit}>
+								<button type="submit" className="btn btn-success mt-3" onSubmit={handleSubmit}>
 									Log in
 									<img src="send-ico.svg" alt="signup" className="ms-2" />
 								</button>
@@ -53,12 +63,7 @@ const Login = () => {
 									<h6>
 										New to MedTracker?{' '}
 										<span className="ms-3">
-											<a href="/signup">
-												<button type="submit" className="btn btn-sm btn-success">
-													Signup
-													<img src="send-ico.svg" alt="signup" className="ms-2" />
-												</button>
-											</a>
+											<a href="/signup">Signup</a>
 										</span>
 									</h6>
 								</div>
--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -14,13 +14,23 @@ const Signup = () => {
 		<div className="container ">
 			<div className="row justify-content-center ">
 				<div className="col-sm-12 col-md-8 col-lg-10">
-					<div className="card">
-						<div>
-							<img src="mt3.jpg" className="entryLogo" alt="logo" />
-							<h2>MedTracker</h2>
+					<div className="card border border-info-subtle rounded-4 shadow-lg">
+						<div className="container">
+							<div className="row">
+								<img src="mt3.jpg" className="entryLogo" alt="logo" />
+
+								<div className="col  align-self-center">
+									<h1 className="fw-bold d-inline-flex m-5" style={{ textDecoration: 'none', color: 'darkcyan' }}>
+										<em>MedTracker</em>
+									</h1>
+									<h5 className="pt-2">
+										<em>Manage and track all your medications seamlessly!</em>
+									</h5>
+								</div>
+							</div>
 						</div>
 						<div className="card-body">
-							<form>
+							<form className="p-3">
 								<div className="mb-3">
 									<label htmlFor="inputFirstName" className="form-label">
 										First Name
@@ -58,7 +68,7 @@ const Signup = () => {
 										value={password}
 									/>
 								</div>
-								<button type="submit" className="btn btn-success" onSubmit={handleSubmit}>
+								<button type="submit" className="btn btn-success mt-3" onSubmit={handleSubmit}>
 									Sign Up
 									<img src="send-ico.svg" alt="signup" className="ms-2" />
 								</button>
