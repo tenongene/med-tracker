0eaed435b6735e0188e2e672ec6e5e8ea72fc0f3
Tenongene <tenongene00@gmail.com>
2023-11-24 18:42:13 -0600

    Hashing, encryption and jwt signing complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
 const _ = require('lodash');
+const validator = require('validator');
 const { randomInt } = require('node:crypto');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+//
+//JWT issue function
+const createToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.EXP });
+};
+//
 //bccrypt hashing
 const hashPass = async (password) => {
 	const salt = await bcrypt.genSalt();
@@ -20,6 +28,8 @@ const {
 	QueryCommand,
 } = require('@aws-sdk/client-dynamodb');
 
+//
+//randomid generator for userId
 const id = () => {
 	return randomInt(1000000);
 };
@@ -63,9 +73,9 @@ const loginUser = (req, res) => {
 };
 
 // signup user
-const signupUser = (req, res) => {
-	console.log(req.body);
+const signupUser = async (req, res) => {
 	const { userName, email, password, firstName } = req.body;
+	const h_pass = await hashPass(password);
 	const userModel = {
 		TableName: TABLE_NAME,
 		Item: {
@@ -73,30 +83,53 @@ const signupUser = (req, res) => {
 				S: `${userName}`,
 			},
 			password: {
-				S: `${password}`,
+				S: `${h_pass}`,
 			},
 			email: {
 				S: `${email}`,
 			},
 			firstName: {
-				S: `${firstName}`,
+				S: `${_.capitalize(firstName)}`,
 			},
 			id: {
 				N: `${id()}`,
 			},
 		},
 	};
-	const newUser = new PutItemCommand(userModel);
-	client
-		.send(newUser)
-		.then((response) => {
-			res.status(200).json(response);
-			console.log(response);
-		})
-		.catch((err) => {
-			res.status(500).json({ error: err.message });
-			console.log(err);
-		});
+
+	try {
+		//validation
+		if (!email || !password || !firstName || !userName) {
+			throw Error('All fields must be filled');
+		}
+
+		if (!validator.isEmail(email)) {
+			throw Error('Email must be a valid email');
+		}
+
+		if (!validator.isStrongPassword(password)) {
+			throw Error('Password must be at least 6 characters in length and contain at least one special character');
+		}
+
+		const newUser = new PutItemCommand(userModel);
+		// console.log(newUser.input.Item.password.S);
+		console.log(h_pass);
+		const token = createToken({ userName });
+		//
+		client
+			.send(newUser)
+			.then((response) => {
+				res.status(200).json({ email, token });
+				console.log(token);
+			})
+			//
+			.catch((err) => {
+				res.status(400).json({ error: err.message });
+				// console.log(err);
+			});
+	} catch (err) {
+		res.status(400).send({ error: err.message });
+	}
 };
 
 module.exports = { signupUser, loginUser };
